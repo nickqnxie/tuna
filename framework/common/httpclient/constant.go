37005ba5e205b7ef869b233f7a4824b0e3e5ed90
nickqnxie <42687780+nickqnxie@users.noreply.github.com>
2021-08-16 00:09:24 +0800

    Create constant.go

---
+++ framework/common/httpclient/constant.go
@@ -0,0 +1,20 @@
+/*
+@Time : 2019/9/20 09:03
+@Author : nickqnxie
+@File : constant.go
+*/
+
+package httpclient
+
+const (
+	MIMEJSON              = "application/json"
+	MIMEHTML              = "text/html"
+	MIMEXML               = "application/xml"
+	MIMEXML2              = "text/xml"
+	MIMEPlain             = "text/plain"
+	MIMEPOSTForm          = "application/x-www-form-urlencoded"
+	MIMEMultipartPOSTForm = "multipart/form-data"
+	MIMEPROTOBUF          = "application/x-protobuf"
+	MIMEMSGPACK           = "application/x-msgpack"
+	MIMEMSGPACK2          = "application/msgpack"
+)
